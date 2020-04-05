922701376e6d66c844a8b12c23ee24d185f11aea
light <qingminlight@gmail.com>
2020-04-05 23:56:51 +0800

    first commit

---
+++ src/CommentApp.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+import CommentInput from './CommentInput';
+import CommentList from './CommnetList';
+
+
+class CommentApp extends Component {
+    render() {
+        return (
+            <div className='wrapper'>
+                <CommentInput />
+                <CommentList />
+            </div>
+        )
+    }
+}
+
+export default CommentApp;
---
+++ src/CommentInput.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class CommentInput extends Component {
+    render () {
+        return (
+          <div className='comment-input'>
+            <div className='comment-field'>
+              <span className='comment-field-name'>用户名：</span>
+              <div className='comment-field-input'>
+                <input className='comment-field-input-input'/ >
+              </div>
+            </div>
+            <div className='comment-field'>
+              <span className='comment-field-name'>评论内容：</span>
+              <div>
+                <textarea className='comment-field-textarea'/>
+              </div>
+            </div>
+            <div>
+              <button className='comment-field-button'>
+                发布
+              </button>
+            </div>
+          </div>
+        )
+      }
+
+}
+export default CommentInput;
