dcd7834455fdd54154fba14a2426dee26dc29bac
light <qingminlight@gmail.com>
2020-04-06 21:52:03 +0800

    whats wrong

--- src/CommnetList.js
+++ src/CommnetList.js
@@ -8,6 +8,7 @@ class CommentList extends Component {
   render() {
     return (
       <div>
+        
         {this.props.comments.map((comment, i) => <Comment comment={comment} key={i} />)}
       </div>
     )
