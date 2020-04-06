23f4d8b55d73f29b3ef321a4e05a4db86fb5a349
light <qingminlight@gmail.com>
2020-04-06 21:40:15 +0800

    finish the first part

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -4,11 +4,25 @@ import CommentList from './CommnetList';
 
 
 class CommentApp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            comments: []
+        }
+    }
+    handleSubmitComment(comment) {
+        if(!comment.content) return alert('请输入内容');
+        if(!comment.username) return alert('请输入姓名')
+        this.state.comments.push(comment)
+        this.setState({
+            comments: this.state.comments
+        })
+    }
     render() {
         return (
             <div className='wrapper'>
-                <CommentInput />
-                <CommentList />
+                <CommentInput onSubmit={this.handleSubmitComment.bind(this)}/>
+                <CommentList comments={this.state.comments}/>
             </div>
         )
     }
--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
 
 class CommentInput extends Component {
+    constructor() {
+      super();
+      this.state = {
+        username: '',
+        content: ''
+      }
+    }
+    handleUsernameChange(event) {
+      this.setState({
+        username: event.target.value
+      })
+    }
+    handleContentChange(event) {
+      this.setState({
+        content: event.target.value
+      })
+    }
+    handleSubmit(event) {
+      if(this.props.onSubmit) {
+        const {username, content} = this.state;
+        this.props.onSubmit({username, content});
+      }
+      this.setState({
+        content: ''
+      })
+    }
+
     render () {
         return (
           <div className='comment-input'>
             <div className='comment-field'>
               <span className='comment-field-name'>用户名：</span>
               <div className='comment-field-input'>
-                <input className='comment-field-input-input'/ >
+                <input className='comment-field-input-input' 
+                      value={this.state.username} 
+                      onChange = {this.handleUsernameChange.bind(this)} / >
               </div>
             </div>
             <div className='comment-field'>
               <span className='comment-field-name'>评论内容：</span>
               <div className="comment-for-nothing">
-                <textarea className='comment-field-textarea'/>
+                <textarea className='comment-field-textarea' 
+                  value={this.state.content} 
+                  onChange={this.handleContentChange.bind(this)}
+                />
               </div>
             </div>
             <div>
-              <button className='comment-field-button'>
+              <button className='comment-field-button' 
+                onClick={this.handleSubmit.bind(this)}
+              >
                 发 布
               </button>
             </div>
@@ -26,4 +60,6 @@ class CommentInput extends Component {
       }
 
 }
+
+
 export default CommentInput;
